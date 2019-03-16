4c1918babd0bb4c7263fa048212d5385a0401260
Malte Z. Andreasen <mzan16@student.aau.dk>
2019-03-16 12:14:38 +0100

    Placeholder now more.. Umm.. Entertaining!

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let i       = 0;
+let j       = 0;
+let  iUp    = {bool: true};
+let  jUp    = {bool: true};
+let cHeight = 600;
+let cWidth  = 800;
+let s       = "Hello!"
+
+function setup() {
+  createCanvas(cWidth, cHeight);
+}
+
+function draw() {
+  background(0);
+
+  fill(255);
+  ellipse(i + 40, j + 40, 80, 80);
+
+  fill(0);
+  textSize(26);
+  text(s, i + 2, j + 35, i + 60, j - 30);
+
+  i = incrementer(i, iUp, cWidth);
+  j = incrementer(j, jUp, cHeight);
+
+}
+
+function incrementer(a, obj, limit) {
+  if (obj.bool) {
+    if (a <= (limit - 80)) {
+      return a + 1;
+    } else {
+      obj.bool = false;
+      return a - 1;
+    }
+  } else {
+    if (a >= 0) {
+      return a - 1;
+    } else {
+      obj.bool = true;
+      return a + 1;
+    }
+  }
+}
